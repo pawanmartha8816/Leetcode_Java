b934b782a15d764d32d4aa9f147d73c3f93e5fea
pawanmartha8816 <pawanmartha16@gmail.com>
2026-04-30 06:40:10 +0530

    Time: 12 ms (2.29%), Space: 46.3 MB (71.76%) - LeetHub

---
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int s=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(n%(i+1)==0){
+                System.out.println(nums[i]);
+                s+=(nums[i]*nums[i]);
+            }
+        }
+        return s;
+    }
+}
